c30bc9e3d3d1add9291e9f251c037539d4cb641c
yui19278 <44569789+yui19278@users.noreply.github.com>
2024-07-04 02:45:19 +0900

    add solution

---
+++ solution.js
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+#define itn int
+#define cotu cout
+using namespace std;
+using ll = long long int;
+
+int main(){
+
+	int N, ans = 0;
+	cin >> N;
+
+
+
+	cout << ans << endl;
+
+}
